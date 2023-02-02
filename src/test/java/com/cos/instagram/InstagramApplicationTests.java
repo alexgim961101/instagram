de549387d49eceffcdbd529cdb8414fb8db18e2c
alexgim <alexgim961101@gmail.com>
2023-02-02 22:22:43 +0900

    setting

--- src/test/java/com/cos/instagram/InstagramApplicationTests.java
+++ src/test/java/com/cos/instagram/InstagramApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cos.photogramstart;
+package com.cos.instagram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhotogramStartApplicationTests {
+class InstagramApplicationTests {
 
 	@Test
 	void contextLoads() {
