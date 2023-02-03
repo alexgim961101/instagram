d7314f7b4a8c644eebe626637fb85f362f3a9560
alexgim <alexgim961101@gmail.com>
2023-02-04 01:37:48 +0900

    공통 응답 Script 만들기

--- src/main/java/com/cos/instagram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/instagram/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.instagram.handler;
 
 import com.cos.instagram.handler.ex.CustomValidationException;
+import com.cos.instagram.util.Script;
 import com.cos.instagram.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,7 +14,11 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)    // CustomValidationException이 발생하는 모든 예외를 가로챔
-    public CMRespDto<Map<String, String>> validationException(CustomValidationException e) {
-        return new CMRespDto(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e) {
+        // CMRespDto, Script 비교
+        // 1. 클라이언트에게 응답할때는 Script가 좋음
+        // 2. Ajax통신 - CMRespDto
+        // 3. Android통신 - CMRespDto
+        return Script.back(e.getErrorMap().toString());
     }
 }
